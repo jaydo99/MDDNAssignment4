f78d406e1849212935295b57c9917c932fc05eab
Jayden Yiu <jaydenyiu@Jaydens-MacBook-Pro.local>
2019-10-08 14:17:18 +1300

    added all code

---
+++ main.js
@@ -0,0 +1,43 @@
+function click1(){
+document.getElementById('pic1').onclick= function(){
+document.getElementById('infobox').innerHTML= '<p> TEST IT </p> <p> IMG: Rugby ball being held </p>'
+}
+}
+
+function click2(){
+document.getElementById('pic2').onclick= function(){
+document.getElementById('infobox').innerHTML= '<p> TEST IT </p> <p> IMG: Rugby ball being held </p> <p> IMG: Tree in a field </p> '
+}
+}
+
+function click3(){
+document.getElementById('pic3').onclick= function(){
+document.getElementById('infobox').innerHTML= '<p> TEST IT </p> <p> IMG: Rugby ball being held </p> <p> IMG: Tree in a field </p> <p>IMG: Basketball hoop </p><p> INTEREST: Sport </p>'
+}
+}
+
+function click4(){
+document.getElementById('pic4').onclick= function(){
+document.getElementById('infobox').innerHTML= '<p> TEST IT </p> <p> IMG: Rugby ball being held </p> <p> IMG: Tree in a field </p> <p>IMG: Basketball hoop </p><p> INTEREST: Sport </p><p>IMG: Camera </p>'
+}
+}
+
+function click5(){
+document.getElementById('pic5').onclick= function(){
+document.getElementById('infobox').innerHTML= '<p> TEST IT </p> <p> IMG: Rugby ball being held </p> <p> IMG: Tree in a field </p> <p>IMG: Basketball hoop </p><p> INTEREST: Sport </p><p>IMG: Camera </p> <p> IMG: Rugby posts </p> <p> INTEREST: Rugby </p>'
+}
+}
+
+function click6(){
+document.getElementById('pic6').onclick= function(){
+document.getElementById('infobox').innerHTML= '<p> TEST IT </p> <p> IMG: Rugby ball being held </p> <p> IMG: Tree in a field </p> <p>IMG: Basketball hoop </p><p> INTEREST: Sport </p><p>IMG: Camera </p> <p> IMG: Rugby posts </p> <p> INTEREST: Rugby </p> <p> IMG: Lake and mountains</p><p> INTEREST: Nature </p>'
+
+}
+}
+
+function click7(){
+document.getElementById('pic7').onclick= function(){
+document.getElementById('infobox').innerHTML= '<p> TEST IT </p> <p> IMG: Rugby ball being held </p> <p> IMG: Tree in a field </p> <p>IMG: Basketball hoop </p><p> INTEREST: Sport </p><p>IMG: Camera </p> <p> IMG: Rugby posts </p> <p> INTEREST: Rugby </p> <p> IMG: Lake and mountains</p><p> INTEREST: Nature </p> <p> IMG: Guitar </p>'
+
+}
+}
